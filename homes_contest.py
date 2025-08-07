430ada83f34fc0ae9bdd04af11c3ef6aa80d22cc
amandacao2 <amndaca0@gmail.com>
2025-08-07 14:40:46 -0400

    first files

---
+++ homes_contest.py
@@ -0,0 +1,3 @@
+#https://skills.yourlearning.ibm.com/activity/MDL-456?planId=PLAN-D8E7C82C1D76&sectionId=SECTION-B&planIdFromParentTab=PLAN-D8E7C82C1D76&sectionIdFromParentTab=SECTION-A&planIdForChildTab=PLAN-D8E7C82C1D76
+#https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques
+import pandas as pd 
--- iowa_homes.py
+++
@@ -1,2 +0,0 @@
-#https://www.kaggle.com/code/dansbecker/basic-data-exploration
-#https://www.kaggle.com/code/scratchpad/notebook13d31dae01/edit
